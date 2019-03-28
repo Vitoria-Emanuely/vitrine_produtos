ae378a1e42373db86d9b3ade4f0b4516644e30ed
Vitória Emanuely <vikaemanuely@gmail.com>
2019-03-28 14:05:17 -0300

    Listagem de produtos

--- vitrineapp/views/index_view.py
+++ vitrineapp/views/index_view.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
@@ -6,4 +7,6 @@ class IndexView(View):
     template = 'index.html'
 
     def get(self, request):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/lista_produtos')
         return render(request, self.template)
--- vitrineapp/views/produto_view.py
+++ vitrineapp/views/produto_view.py
@@ -1,25 +1,31 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import View
 
 from vitrineapp.forms import ProdutoForm
+from vitrineapp.models import ProdutoModel
 
 
 class ListaProdutoView(View):
     template = 'lista_produtos.html'
 
     def get(self, request):
-        #lista_produtos = ProdutoModel.objects.all()
-        #objeto = {
-        #'lista_produtos': lista_produtos
-        #}
+        lista_produtos = ProdutoModel.objects.all()
+        objeto = {
+            'lista_produtos': lista_produtos
+        }
         #TODO
-        return render(request, self.template)
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        return render(request, self.template, objeto)
 
 class CadastroProdutoView(View):
     template = 'cadastro_produtos.html'
 
     def get(self, request):
         form = ProdutoForm
+        if not request.user.is_authenticated:
+            HttpResponseRedirect('')
         return render(request, self.template, {'form': form})
 
     def post(self, request):
